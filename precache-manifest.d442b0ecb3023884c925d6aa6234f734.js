3adda570253e10585723bc2b9a87a529bbd98637
Siddharth Upamanyu <siddharthupamanyu@gmail.com>
2019-10-28 11:45:46 +0530

    Updates

---
+++ precache-manifest.d442b0ecb3023884c925d6aa6234f734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b767328645bce345a779d49c2f844d",
+    "url": "/installment-calculator/index.html"
+  },
+  {
+    "revision": "272a82d620e47decd512",
+    "url": "/installment-calculator/static/css/main.adc20d2b.chunk.css"
+  },
+  {
+    "revision": "e082f581199e7439fd21",
+    "url": "/installment-calculator/static/js/2.0bf77397.chunk.js"
+  },
+  {
+    "revision": "272a82d620e47decd512",
+    "url": "/installment-calculator/static/js/main.cc6fd62a.chunk.js"
+  },
+  {
+    "revision": "5fe4704952e6b2ddbe0c",
+    "url": "/installment-calculator/static/js/runtime-main.7edc42c6.js"
+  }
+]);
